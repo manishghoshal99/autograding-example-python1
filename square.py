8a8961a772b4040f3686fdd1411667ffd9a99f73
Roopashree S <46236083+Roopashaily@users.noreply.github.com>
2022-02-08 20:58:00 +0530

    Updated to insert the new code to solve assignment-3

--- square.py
+++ square.py
@@ -1,2 +1,10 @@
 def squaring_num():
-    return "Hello!"
+    # Type your code to read four values here.
+
+
+    # Output a short story using the four input values. Do not modify the code below.
+    output_string=first_name+" "+'went to'+" "+ generic_location+" "+'to buy'+" "+str(whole_number)+" "+'different types of'+" "+plural_noun
+    print(first_name, 'went to', generic_location, 'to buy', whole_number, 'different types of', plural_noun)
+
+    #return the final output_string variable without fail
+    return
